a1da5f9e924069751d44fd6cdc64a828be5a70eb
camilo.echeverry42956@ucaldas.edu.co <camilo.echeverry42956@ucaldas.edu.co>
2025-05-26 17:52:39 -0500

    Sexto commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
